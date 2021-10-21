6fea1879144aae980554c7553d6508c6ef704788
MikeZeg <michalzegzula@gmail.com>
2021-10-21 21:32:10 +0100

    Array function

--- Chrome_Extension/javascript.js
+++ Chrome_Extension/javascript.js
@@ -2,20 +2,30 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 
 
-document.getElementById("input-btn").addEventListener("click", ()=>{
-  myLeads.push(inputEl.value);
-});
+// document.getElementById("input-btn").addEventListener("click", ()=>{
+//   console.log("Press");
 //
-// for(i = 0; i < myLeads.lenght; i++){
-//   console.log(myLeads[i]);
-// }
+//   const newText = document.getElementById("input-el").value;
+//   myLeads += [myLeads.push(newText)];
+//
+//   console.log(newText);
+//   console.log(myLeads);
+// });
 
+document.getElementById("input-btn").addEventListener("click", function (txt){
 
+  let newText = document.getElementById("input-el");//Catch a value and change to string and trim space
+  let newTextString = newText.value.toString();
+  newTextString = newTextString.trim();
 
+  myLeads.push(newTextString);//add txt to array
 
+  //check the array
+  // for (i=0; i<myLeads.length; i++){
+  //   console.log("Przycisk nr." + [i+1] + ": " + myLeads[i] + " ");
+  // }
 
-
-
+});
 
 //task
 // document.getElementById('box-opend').addEventListener("click", () => {

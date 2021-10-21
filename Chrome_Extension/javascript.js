6b304624e6ba69d0d617bc413780d6e9018a1f17
MikeZeg <michalzegzula@gmail.com>
2021-10-21 18:54:58 +0100

    New app files

---
+++ Chrome_Extension/javascript.js
@@ -0,0 +1,22 @@
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
+
+
+document.getElementById("input-btn").addEventListener("click", ()=>{
+  myLeads.push(inputEl.value);
+});
+//
+// for(i = 0; i < myLeads.lenght; i++){
+//   console.log(myLeads[i]);
+// }
+
+
+
+
+
+
+
+
+//task
+// document.getElementById('box-opend').addEventListener("click", () => {
+//   console.log('I want open the box')});

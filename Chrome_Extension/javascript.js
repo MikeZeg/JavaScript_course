01321d8b94afbb14fcf7e6ce1182f68cd878fd53
MikeZeg <michalzegzula@gmail.com>
2021-10-24 20:44:24 +0100

    Extension adds plus new feature

--- Chrome_Extension/javascript.js
+++ Chrome_Extension/javascript.js
@@ -1,32 +1,59 @@
+//chrome://extensions/
+
+// array
 let myLeads = [];
+
+//Variables
+
+let text = "";
+
+
+
+//Catcher Elements
 const inputEl = document.getElementById('input-el');
+const ulEl = document.getElementById("ul-el");
 
 
-// document.getElementById("input-btn").addEventListener("click", ()=>{
-//   console.log("Press");
-//
-//   const newText = document.getElementById("input-el").value;
-//   myLeads += [myLeads.push(newText)];
-//
-//   console.log(newText);
-//   console.log(myLeads);
-// });
+document.getElementById("input-btn").addEventListener("click", function (){
 
-document.getElementById("input-btn").addEventListener("click", function (txt){
-
-  let newText = document.getElementById("input-el");//Catch a value and change to string and trim space
-  let newTextString = newText.value.toString();
-  newTextString = newTextString.trim();
-
-  myLeads.push(newTextString);//add txt to array
-
-  //check the array
-  // for (i=0; i<myLeads.length; i++){
-  //   console.log("Przycisk nr." + [i+1] + ": " + myLeads[i] + " ");
-  // }
-
+  myLeads.push(inputEl.value);
+  renderLeads();
+  inputEl.value = "";
 });
 
-//task
-// document.getElementById('box-opend').addEventListener("click", () => {
-//   console.log('I want open the box')});
+function renderLeads(){
+    let listItem = "";
+
+    for (i=0; i<myLeads.length; i++){
+      console.log("Przycisk nr." + [i+1] + ": " + myLeads[i] + " ");
+
+      // listItem +='<li>' + '<a href="https://' + myLeads[i] + '"target="_blank">' + myLeads[i] +'</a>' + '</li>';
+      listItem +=`
+      <li>
+          <a href = "https://${myLeads[i]}" target="_blank">
+            ${myLeads[i]}
+          </a>
+      </li>
+      `
+
+
+      //Other methods to create elements
+      // const li = document.createElement("li");
+      // li.textContent = myLeads[i];
+      // ulEl.append(li);
+    }
+    ulEl.innerHTML = listItem;
+}
+
+
+//Task
+
+const recipient = "James";
+const sender = "Michal";
+
+const email =  `
+  Hey ${recipient}!
+  How its going?
+  Cheers ${sender}.
+`
+console.log(email);

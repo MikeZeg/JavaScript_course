9232a07641e7b3d8572738fba3f94331b50a2017
MikeZeg <michalzegzula@gmail.com>
2021-10-29 18:40:10 +0100

    update - Save btn

--- Chrome_Extension/javascript.js
+++ Chrome_Extension/javascript.js
@@ -2,6 +2,10 @@
 
 // array
 let myLeads = [];
+const tab = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+];
+
 
 
 //Variables
@@ -18,50 +22,55 @@ const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById("ul-el");
 const inputBtn = document.getElementById("input-btn");
 const deletBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
 //Action - Event listener
 inputBtn.addEventListener("click", function (){
 
   myLeads.push(inputEl.value);
-  renderLeads();
+  render(myLeads);
   inputEl.value = "";
 
-//Save in local Storage
+                          //Save in local Storage
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
 });
 
 deletBtn.addEventListener("dblclick", function (){
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
 
+tabBtn.addEventListener("click", function(){
+  console.log(tab[0].url);
+});
 
 //Function
-function renderLeads(){
+
+if (leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function render(leads){
     let listItem = "";
 
-    for (i=0; i<myLeads.length; i++){
-      console.log("Przycisk nr." + [i+1] + ": " + myLeads[i] + " ");
+    for (i=0; i< leads.length ; i++){
+      console.log("Przycisk nr." + [i+1] + ": " + leads[i] + " ");
 
-      // listItem +='<li>' + '<a href="https://' + myLeads[i] + '"target="_blank">' + myLeads[i] +'</a>' + '</li>';
+      // listItem +='<li>' + '<a href="https://' + leads[i] + '"target="_blank">' + leads[i] +'</a>' + '</li>';
       listItem +=`
       <li>
-          <a href = "https://${myLeads[i]}" target="_blank">
-            ${myLeads[i]}
+          <a href = "https://${leads[i]}" target="_blank">
+            ${leads[i]}
           </a>
       </li>
       `
-
-
       //Other methods to create elements
       // const li = document.createElement("li");
       // li.textContent = myLeads[i];
       // ulEl.append(li);
     }
+
     ulEl.innerHTML = listItem;
-}
-if (leadsFromLocalStorage){
-  myLeads = leadsFromLocalStorage;
-  renderLeads();
 }
